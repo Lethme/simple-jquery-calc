62a44825bdf9e3928330cfe8dc0aadda4d079c7e
Ashe <63866529+Lethme@users.noreply.github.com>
2021-11-14 15:22:20 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,218 @@
+$(() => {
+    let calc = $('.calc');
+    let display = $('.calc .calc-display');
+    let btns = $('.calc .calc-btns');
+    let initialState = true;
+
+    let displayValue = "0";
+    let evalValue = "0";
+
+    let createBtnTemplate = btn => {
+        return `
+            <div class="btn-wrapper col-` + btn.size + `">
+                <div class="btn` + (btn.class !== "" ? (" " + btn.class) : "") + `">` + btn.symbol + `</div>
+            </div>
+        `
+    };
+
+    const BtnType = Object.freeze({
+        InputBtn: 0,
+        FuncBtn: 1,
+    });
+
+    let buttons = [{
+        symbol: "(",
+        displayedSymbol: "(",
+        size: 3,
+        type: BtnType.InputBtn,
+        class: "",
+        getTemplate: function() { return createBtnTemplate(this) }
+    }, {
+        symbol: ")",
+        displayedSymbol: ")",
+        size: 3,
+        type: BtnType.InputBtn,
+        class: "",
+        getTemplate: function() { return createBtnTemplate(this) }
+    }, {
+        symbol: "C",
+        size: 3,
+        type: BtnType.FuncBtn,
+        class: "clr",
+        getTemplate: function() { return createBtnTemplate(this) },
+        func: function() {
+            displayValue = "0";
+            evalValue = "0";
+            initialState = true;
+            updateDisplay();
+        }
+    }, {
+        symbol: "/",
+        displayedSymbol: "/",
+        size: 3,
+        type: BtnType.InputBtn,
+        class: "div",
+        getTemplate: function() { return createBtnTemplate(this) }
+    }, {
+        symbol: "7",
+        displayedSymbol: "7",
+        size: 3,
+        type: BtnType.InputBtn,
+        class: "",
+        getTemplate: function() { return createBtnTemplate(this) }
+    }, {
+        symbol: "8",
+        displayedSymbol: "8",
+        size: 3,
+        type: BtnType.InputBtn,
+        class: "",
+        getTemplate: function() { return createBtnTemplate(this) }
+    }, {
+        symbol: "9",
+        displayedSymbol: "9",
+        size: 3,
+        type: BtnType.InputBtn,
+        class: "",
+        getTemplate: function() { return createBtnTemplate(this) }
+    }, {
+        symbol: "*",
+        displayedSymbol: "x",
+        size: 3,
+        type: BtnType.InputBtn,
+        class: "mul",
+        getTemplate: function() { return createBtnTemplate(this) }
+    }, {
+        symbol: "4",
+        displayedSymbol: "4",
+        size: 3,
+        type: BtnType.InputBtn,
+        class: "",
+        getTemplate: function() { return createBtnTemplate(this) }
+    }, {
+        symbol: "5",
+        displayedSymbol: "5",
+        size: 3,
+        type: BtnType.InputBtn,
+        class: "",
+        getTemplate: function() { return createBtnTemplate(this) }
+    }, {
+        symbol: "6",
+        displayedSymbol: "6",
+        size: 3,
+        type: BtnType.InputBtn,
+        class: "",
+        getTemplate: function() { return createBtnTemplate(this) }
+    }, {
+        symbol: "-",
+        displayedSymbol: "-",
+        size: 3,
+        type: BtnType.InputBtn,
+        class: "sub",
+        getTemplate: function() { return createBtnTemplate(this) }
+    }, {
+        symbol: "1",
+        displayedSymbol: "1",
+        size: 3,
+        type: BtnType.InputBtn,
+        class: "",
+        getTemplate: function() { return createBtnTemplate(this) }
+    }, {
+        symbol: "2",
+        displayedSymbol: "2",
+        size: 3,
+        type: BtnType.InputBtn,
+        class: "",
+        getTemplate: function() { return createBtnTemplate(this) }
+    }, {
+        symbol: "3",
+        displayedSymbol: "3",
+        size: 3,
+        type: BtnType.InputBtn,
+        class: "",
+        getTemplate: function() { return createBtnTemplate(this) }
+    }, {
+        symbol: "+",
+        displayedSymbol: "+",
+        size: 3,
+        type: BtnType.InputBtn,
+        class: "add",
+        getTemplate: function() { return createBtnTemplate(this) }
+    }, {
+        symbol: "0",
+        displayedSymbol: "0",
+        size: 6,
+        type: BtnType.InputBtn,
+        class: "",
+        getTemplate: function() { return createBtnTemplate(this) }
+    }, {
+        symbol: ".",
+        displayedSymbol: ".",
+        size: 3,
+        type: BtnType.InputBtn,
+        class: "",
+        getTemplate: function() { return createBtnTemplate(this) }
+    }, {
+        symbol: "=",
+        size: 3,
+        type: BtnType.FuncBtn,
+        class: "eq",
+        getTemplate: function() { return createBtnTemplate(this) },
+        func: function() {
+            let newValue = "";
+            try {
+                newValue = new String(eval(evalValue));
+            } catch (ex) {
+                newValue = "NaN";
+            }
+
+            if (displayValue !== newValue.toString()) initialState = true;
+            displayValue = newValue;
+            evalValue = newValue;
+            updateDisplay();
+        }
+    }];
+
+    let insertSymbol = btn => {
+        if (btn.symbol !== "0" || evalValue !== "0") {
+            if (initialState) {
+                displayValue = "";
+                evalValue = "";
+                initialState = false;
+            }
+
+            displayValue += btn.displayedSymbol;
+            evalValue += btn.symbol;
+        }
+        updateDisplay();
+    }
+
+    let updateDisplay = () => {
+        display.empty();
+        display.html(displayValue);
+    }
+
+    let updateCalcUI = () => {
+        btns.empty();
+
+        let i = 0;
+        buttons.forEach(btn => {
+            btns.append(btn.getTemplate());
+            let btnElement = btns.children().eq(i++).children().eq(0);
+            switch (btn.type) {
+                case BtnType.InputBtn:
+                    {
+                        btnElement.on('click', e => insertSymbol(btn));
+                        break;
+                    }
+                case BtnType.FuncBtn:
+                    {
+                        btnElement.on('click', e => btn.func());
+                        break;
+                    }
+            }
+        });
+    };
+
+    updateCalcUI();
+    updateDisplay();
+});
